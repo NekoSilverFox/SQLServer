7d38994b12d8a0a10464104b837a9515798ccab2
foxthere <weidufox@gmail.com>
2021-09-26 18:40:55 +0300

    优化 表的创建过程的笔记

--- 001 - 创建爱数据库.sql
+++ 001 - 创建爱数据库.sql
@@ -52,5 +52,19 @@ log on
 	-- maxsize=100mb 日志一般不限制最大容量
 )
 
+
 -------------------------------------------------------------------------
--- 创建数据表
+-- 创建数据表（Teacher）ID Name Gender Age Salary Birthday
+use TestSchool											-- 首先要指明所属的数据库
+if exists(select * from sysobjects where name='Teacher') -- 查询表要用 sysobjects 关键字
+	Drop table Teacher
+go
+create table Teacher
+(
+	ID int identity(1,1) primary key,					--【设置表示列】identity(标识种子,标识增量)
+	Name nvarchar not null,								--【not null】标记字段不能为空
+	Gender bit not null,
+	Age smallint check(age>=0 and age<=100) not null,	--【check】设置check约束
+	Salary money,										-- 当一个字段可以为null时，可以写 null，也可以不写
+	Birthday datetime not null default('2000-1-1')		--【default()】默认值
+)
