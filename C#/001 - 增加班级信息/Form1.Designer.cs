8265b2affb9fc00459f2eb27c704ad6ee76bce3f
冰糖雪狸 <46406730+NekoSilverFox@users.noreply.github.com>
2021-10-22 00:03:55 +0300

    增加 ADO示例 - 添加班级

--- C#/001 - 增加班级信息/Form1.Designer.cs
+++ C#/001 - 增加班级信息/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.textClassName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(375, 207);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "新增";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // textClassName
+            // 
+            this.textClassName.Location = new System.Drawing.Point(225, 152);
+            this.textClassName.Name = "textClassName";
+            this.textClassName.Size = new System.Drawing.Size(365, 23);
+            this.textClassName.TabIndex = 1;
+            this.textClassName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.textClassName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox textClassName;
     }
 }
 
--- C#/001 - 增加班级信息/Form1.cs
+++ C#/001 - 增加班级信息/Form1.cs
@@ -8,6 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// 当我们创建的对象里面有 sql 三个字的就要引入这个命名空间
+using System.Data.SqlClient;
+
+
 namespace _001___增加班级信息
 {
     public partial class Form1 : Form
@@ -17,5 +21,64 @@ namespace _001___增加班级信息
             InitializeComponent();
         }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        /*
+         * 1. 创建数据库链接（服务器名称 身份验证）
+         * 2. 点击链接打开数据库链接
+         * 3. 切换当前操作的数据库
+         * 4. 创建SQL命令
+         * 5. 点击 “执行” --> 返回值
+         * 6. 判断
+         * 7. 关闭链接
+         */
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // 1. 创建连接通道
+            SqlConnection conn = new SqlConnection();
+
+            // 2. 告诉连接通道具体如何进行连接
+            string connStr = "server=DESKTOP-HMF772I\\SQLSERVER;uid=sa;pwd=123;database=CZSchool";
+
+            // 3. 指定连接通道如何进行连接
+            conn.ConnectionString = connStr;
+
+            // 4. 因为连接的是一个对象，还需要打开
+            conn.Open();
+
+            // 5. 创建你需要执行的SQL命令
+            //string sql = "insert into grade values('"+txtClassName.Text.Trim()+"')";
+            string sql = string.Format("insert into grade values('{0}')", textClassName.Text.Trim());
+
+            // 6. 创建命令 "执行" 对象：这只是一个命令的传递者，告诉他：去哪里，做什么
+            SqlCommand comm = new SqlCommand();
+
+            // 7. 指定命令怎么走
+            comm.Connection = conn;
+
+            // 8. 指定要传递的命令语句
+            comm.CommandText = sql;
+
+            // 9. 让命令对象“执行”命令，同时接收从服务器返回的值
+            int num = comm.ExecuteNonQuery();
+
+            // 10. 对返回的值进行判断
+            if (num == 1)
+            {
+                MessageBox.Show("增加成功");
+            }
+            else
+            {
+                MessageBox.Show("增加失败");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
