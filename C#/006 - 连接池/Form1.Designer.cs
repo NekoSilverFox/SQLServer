1a4a2801050686a735b428362da8876d8b4ec4e2
冰糖雪狸 <46406730+NekoSilverFox@users.noreply.github.com>
2021-10-27 00:00:54 +0300

    增加 连接池

---
+++ C#/006 - 连接池/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace _006___连接池
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnection = new System.Windows.Forms.Button();
+            this.btnPool = new System.Windows.Forms.Button();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConnection
+            // 
+            this.btnConnection.Location = new System.Drawing.Point(214, 110);
+            this.btnConnection.Name = "btnConnection";
+            this.btnConnection.Size = new System.Drawing.Size(295, 23);
+            this.btnConnection.TabIndex = 0;
+            this.btnConnection.Text = "第一个对象 Connection";
+            this.btnConnection.UseVisualStyleBackColor = true;
+            this.btnConnection.Click += new System.EventHandler(this.btnConnection_Click);
+            // 
+            // btnPool
+            // 
+            this.btnPool.Location = new System.Drawing.Point(214, 186);
+            this.btnPool.Name = "btnPool";
+            this.btnPool.Size = new System.Drawing.Size(295, 23);
+            this.btnPool.TabIndex = 1;
+            this.btnPool.Text = "连接池";
+            this.btnPool.UseVisualStyleBackColor = true;
+            this.btnPool.Click += new System.EventHandler(this.btnPool_Click);
+            // 
+            // btnChange
+            // 
+            this.btnChange.Location = new System.Drawing.Point(214, 259);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(295, 23);
+            this.btnChange.TabIndex = 2;
+            this.btnChange.Text = "触发事件";
+            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnChange);
+            this.Controls.Add(this.btnPool);
+            this.Controls.Add(this.btnConnection);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnection;
+        private System.Windows.Forms.Button btnPool;
+        private System.Windows.Forms.Button btnChange;
+    }
+}
+
---
+++ C#/006 - 连接池/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace _006___连接池
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnection_Click(object sender, EventArgs e)
+        {
+            String connStr = "Data Source=DESKTOP-HMF772I\\SQLSERVER;Initial Catalog=CZSchool;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(connStr);
+
+            conn.Open();
+            MessageBox.Show("数据库链接已创建");
+
+            // 释放连接通道。但其实没有真正的释放这个对象，只是将他的状态关闭，同时将连接字符串清空
+            conn.Dispose();
+            // 如果需要恢复连接重新将连接字符串赋值即可
+            conn.ConnectionString = connStr;
+
+            conn.Close();
+            MessageBox.Show("数据库链接已关闭");
+        }
+
+        private void btnPool_Click(object sender, EventArgs e)
+        {
+            // pooling=false 不使用连接池
+            String connStr = "Data Source=DESKTOP-HMF772I\\SQLSERVER;Initial Catalog=CZSchool;Integrated Security=True;pooling=false";
+
+            // 秒表
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < 1000; i++)
+            {
+                SqlConnection conn = new SqlConnection(connStr);
+                string sql = "Select * from grade";
+                conn.Open();
+                SqlCommand comm = new SqlCommand(sql, conn);
+                comm.ExecuteNonQuery();
+                conn.Close();
+            }
+            sw.Stop();
+            MessageBox.Show(sw.Elapsed.ToString());
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            String connStr = "Data Source=DESKTOP-HMF772I\\SQLSERVER;Initial Catalog=CZSchool;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(connStr);
+
+            conn.StateChange += Conn_StateChange; // += 后按两下 Tab 键自动创建
+            conn.Open();
+            conn.Close();
+            conn.Open();
+        }
+
+        // 事件（句柄）
+        private void Conn_StateChange(object sender, StateChangeEventArgs e)
+        {
+            //                  上个事件                    当前事件
+            MessageBox.Show(e.OriginalState + " " + e.CurrentState);
+        }
+
+
+    }
+}
