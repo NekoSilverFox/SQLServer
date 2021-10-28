b89f7529c7a7949d1b3b72c26b201eb9df1f13d3
冰糖雪狸 <46406730+NekoSilverFox@users.noreply.github.com>
2021-10-28 18:08:51 +0300

    增加 数据的导出和导入

---
+++ C#/009 - 数据的导出和导入/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace _009___数据的导出和导入
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOutput = new System.Windows.Forms.Button();
+            this.btnInput = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOutput
+            // 
+            this.btnOutput.Location = new System.Drawing.Point(111, 57);
+            this.btnOutput.Name = "btnOutput";
+            this.btnOutput.Size = new System.Drawing.Size(140, 23);
+            this.btnOutput.TabIndex = 0;
+            this.btnOutput.Text = "导出 grade";
+            this.btnOutput.UseVisualStyleBackColor = true;
+            this.btnOutput.Click += new System.EventHandler(this.btnOutput_Click);
+            // 
+            // btnInput
+            // 
+            this.btnInput.Location = new System.Drawing.Point(111, 129);
+            this.btnInput.Name = "btnInput";
+            this.btnInput.Size = new System.Drawing.Size(140, 23);
+            this.btnInput.TabIndex = 0;
+            this.btnInput.Text = "导入 grade";
+            this.btnInput.UseVisualStyleBackColor = true;
+            this.btnInput.Click += new System.EventHandler(this.btnInput_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 215);
+            this.Controls.Add(this.btnInput);
+            this.Controls.Add(this.btnOutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOutput;
+        private System.Windows.Forms.Button btnInput;
+    }
+}
+
---
+++ C#/009 - 数据的导出和导入/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace _009___数据的导出和导入
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOutput_Click(object sender, EventArgs e)
+        {
+            string connStr = "Data Source=DESKTOP-HMF772I\\SQLSERVER;Initial Catalog=CZSchool;Integrated Security=True";
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                using (StreamWriter streamWriter = new StreamWriter("grade.txt"))
+                {
+                    conn.Open();
+                    string sql = "SELECT Classid, ClassName FROM Grade";
+                    SqlCommand comm = new SqlCommand(sql, conn);
+                    SqlDataReader reader = comm.ExecuteReader();
+                    while(reader.Read())
+                    {
+                        streamWriter.WriteLine(reader["Classid"] + "|" + reader["ClassName"]);
+                    }
+                    MessageBox.Show("导出成功");
+                }
+            }
+
+
+        }
+
+        private void btnInput_Click(object sender, EventArgs e)
+        {
+            using (StreamReader streamReader = new StreamReader("grade.txt"))
+            {
+                // 先读出一行
+                string line = streamReader.ReadLine();
+                // 如果这一行不是空字符串或者null，就做相应的处理
+                while (!string.IsNullOrEmpty(line))
+                {
+                    // 做一个字符串分割
+                    string[] split = line.Split('|');
+
+                    // 自己写一个函数方便我们插入
+                    InsertClassName(split[1]);
+
+                    // 【重点】一定要记得，读取下一行
+                    line = streamReader.ReadLine();
+                }
+
+                MessageBox.Show("导入成功");
+            }
+
+        }
+
+
+        void InsertClassName(string classname)
+        {
+            string connStr = "Data Source=DESKTOP-HMF772I\\SQLSERVER;Initial Catalog=CZSchool;Integrated Security=True";
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                string sql = "INSERT INTO Grade VALUES(@ClassName)";
+                SqlParameter parClassname = new SqlParameter("@ClassName", classname);
+                SqlCommand comm = new SqlCommand(sql, conn);
+                comm.Parameters.Add(parClassname);
+                comm.ExecuteNonQuery();
+            }
+        }
+    }
+}
