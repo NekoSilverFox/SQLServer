0b282b7ee5c3ed006056f6613f57f742f56932dc
冰糖雪狸 <46406730+NekoSilverFox@users.noreply.github.com>
2021-10-30 01:09:17 +0300

    增加 EXTIST练习

--- 120 - 多表连接查询练习.sql
+++ 120 - 多表连接查询练习.sql
@@ -152,10 +152,18 @@ SELECT Distinct Disciplines.[Name], Uplans.Semestr
 
 
 --	3.8. Придумайте запрос, который использует операцию соединения по неравенству
+-- 输出2014.1.10后之后考试的学生
+-- Студенты, сдающие экзамены после 10.1.2014
+SELECT Students.NumSt, Students.FIO, Students.NumGr, Balls.DateEx
+	FROM Balls
+		INNER JOIN Students
+			ON Balls.NumSt=Students.NumSt
+		WHERE Balls.DateEx > '2014.1.10'
 
 
 --	3.9. Придумайте запрос, который использует операцию внешнего соединения справа, предварительно подготовив тестовые данные.
 -- 哪个专业的学生参加的考试次数最多
+-- Какие дисциплины сдают больше всего экзаменов
 SELECT TOP 1 Uplans.NumDir
 	FROM Uplans
 	RIGHT JOIN Balls
@@ -180,13 +188,235 @@ SELECT Students.NumGr, COUNT(*) AS 'Количество студентов'
 
 --4. Составьте запросы с использованием подзапросов, в том числе с предикатами EXISTS и  NOT EXISTS
 --	4.1. Выберите студентов, которые сдали только одну дисциплину
+SELECT * 
+	FROM Students AS tb_Stu
+	WHERE EXISTS
+		(
+		SELECT * 
+			FROM Balls 
+			WHERE tb_Stu.NumSt=Balls.NumSt
+			GROUP BY NumSt
+			HAVING COUNT(NumSt)=1
+		)
 
 --	4.2. Выбрать студентов, которые не сдали ни одного экзамена
---	4.3. Выберите группы, в которых есть студенты, сдавшие все экзамены 1 семестра
---	4.4. Выберите группы, в которых есть студенты, которые не сдали ни одной дисциплины
---	4.5. Выбрать дисциплины, которые не попали в учебный план направления 231000
---	4.6. Выбрать дисциплины, которые не сдали все студенты направления 231000
+SELECT * 
+	FROM Students AS tb_Stu
+	WHERE NOT EXISTS
+		(SELECT * 
+			FROM Balls 
+			WHERE tb_Stu.NumSt=Balls.NumSt)
+
+--	4.3. Выберите группы, в которых есть студенты, сдавшие 【все】 экзамены 1 семестра
+-- 从 Uplans 中找第1学期的科目
+SELECT * FROM Uplans WHERE Semestr=1
+SELECT Uplans.IdDisc FROM Uplans WHERE Semestr=1
+
+
+-- 从 Balls 中找参加了第一学期考试的学生
+SELECT * FROM Balls AS tb_B
+	WHERE EXISTS (SELECT Uplans.IdDisc FROM Uplans WHERE Semestr=1 AND tb_B.IdDisc=Uplans.IdDisc) 
+
+-- 参加了所有考试学生的 NumSt
+--SELECT * 
+--	FROM Balls,
+--		(SELECT * FROM Balls AS tb_B WHERE EXISTS (SELECT Uplans.IdDisc FROM Uplans WHERE Semestr=1 AND tb_B.IdDisc=Uplans.IdDisc)) AS tmp_B
+--	WHERE 
+		
+
+--	4.4. Выберите группы, в которых есть студенты, которые 【не】 сдали 【ни одной】 дисциплины
+SELECT Distinct NumGr FROM Students
+	WHERE NOT EXISTS (SELECT NumSt FROM Balls WHERE Balls.NumSt=Students.NumSt)
+
+--	4.5. Выбрать дисциплины, которые не попали в учебный план направления 231000 选择不属于231000教学大纲的学科
+SELECT [NAME] FROM Disciplines
+	WHERE NOT EXISTS (SELECT NumDisc FROM Uplans WHERE Disciplines.NumDisc=Uplans.NumDisc AND Uplans.NumDir='231000')
+
+
+--	4.6. Выбрать дисциплины, которые не сдали все студенты направления 231000 选择所有231000名学生没有通过的科目
+SELECT * 
+	FROM Uplans
+	INNER JOIN Balls
+		ON Uplans.IdDisc=Balls.IdDisc
+	INNER JOIN Students
+		ON Students.NumSt=Balls.NumSt
+	INNER JOIN Groups
+		ON Groups.NumGr=Students.NumGr
+	RIGHT JOIN Disciplines
+		ON Disciplines.NumDisc=Uplans.NumDisc
+	WHERE Groups.NumDir='231000'
+
+SELECT DISTINCT Disciplines.[Name]
+	FROM Uplans
+	INNER JOIN Balls
+		ON Uplans.IdDisc=Balls.IdDisc
+	INNER JOIN Students
+		ON Students.NumSt=Balls.NumSt
+	INNER JOIN Groups
+		ON Groups.NumGr=Students.NumGr
+	RIGHT JOIN Disciplines
+		ON Disciplines.NumDisc=Uplans.NumDisc
+	WHERE Groups.NumDir='231000'
+
+
+SELECT * FROM Disciplines
+	WHERE NOT EXISTS
+	(
+		SELECT *
+			FROM 
+			(
+				SELECT DISTINCT Disciplines.[Name]
+					FROM Uplans
+					INNER JOIN Balls
+						ON Uplans.IdDisc=Balls.IdDisc
+					INNER JOIN Students
+						ON Students.NumSt=Balls.NumSt
+					INNER JOIN Groups
+						ON Groups.NumGr=Students.NumGr
+					RIGHT JOIN Disciplines
+						ON Disciplines.NumDisc=Uplans.NumDisc
+					WHERE Groups.NumDir='231000'
+			) temp
+		WHERE temp.[Name]=Disciplines.[Name]
+	)
+
+
 --	4.7. Выбрать группы, в которых все студенты сдали физику
---	4.8. Выбрать группы, в которых все студенты сдали все дисциплины 1 семестра
+-- 先找到交了物理的所有人，也和他们的班级关联起来
+SELECT * 
+	FROM Balls
+	INNER JOIN Uplans
+		ON Balls.IdDisc=Uplans.IdDisc
+	INNER JOIN Disciplines
+		ON Uplans.NumDisc=Disciplines.NumDisc
+	WHERE Disciplines.[Name]='Физика'
+
+
+
+
+SELECT Students.NumGr 
+	FROM Students
+	INNER JOIN Groups
+		ON Groups.NumGr=Students.NumGr
+	WHERE EXISTS
+	(
+		SELECT *
+			FROM 
+			(
+				SELECT NumSt 
+					FROM Balls
+					INNER JOIN Uplans
+						ON Balls.IdDisc=Uplans.IdDisc
+					INNER JOIN Disciplines
+						ON Uplans.NumDisc=Disciplines.NumDisc
+					WHERE Disciplines.[Name]='Физика'
+			) temp
+			WHERE temp.NumSt=Students.NumSt
+	)
+	GROUP BY Students.NumGr, Groups.Quantity
+	HAVING COUNT(Students.NumGr)=Groups.Quantity
+
+--	4.8. Выбрать группы, в которых все студенты сдали все дисциплины 1 семестра 选择所有学生在第一学期通过所有科目的小组
+-- 找第一学期的科目
+SELECT * FROM Uplans WHERE Semestr='1' ORDER BY NumDir
+
+-- ★ 找第一学期的科目NumDir，和他们的考试数量
+SELECT NumDir, COUNT(NumDir) AS dirBallTimes FROM Uplans WHERE Semestr='1' GROUP BY NumDir
+
+-- 通过第一学期科目的学生
+SELECT * 
+	FROM Balls,
+		(SELECT * FROM Uplans WHERE Semestr='1') AS firstSem
+	WHERE firstSem.IdDisc=Balls.IdDisc
+	ORDER BY Balls.NumSt
+	
+--  通过第一学期科目的学生的ID，并且他们交了多少次考试 
+SELECT Balls.NumSt, firstSem.NumDir, COUNT(Balls.NumSt) AS stuBallTimes
+	FROM Balls,
+		(SELECT * FROM Uplans WHERE Semestr='1') AS firstSem
+	WHERE firstSem.IdDisc=Balls.IdDisc
+	GROUP BY Balls.NumSt, firstSem.NumDir
+	ORDER BY Balls.NumSt
+
+
+-- ★★ 给他加个班级号
+SELECT Balls.NumSt, Students.NumGr, firstSem.NumDir, COUNT(Balls.NumSt) AS stuBallTimes
+	FROM Balls,
+		Students,
+		(SELECT * FROM Uplans WHERE Semestr='1') AS firstSem
+	WHERE firstSem.IdDisc=Balls.IdDisc AND Balls.NumSt=Students.NumSt
+	GROUP BY Balls.NumSt,Students.NumGr ,firstSem.NumDir
+	--ORDER BY Balls.NumSt
+
+
+
+-- ★★★ 输出合适的班级
+SELECT NumSt, NumGr, P2.NumDir
+	FROM 
+	(
+		SELECT NumDir, COUNT(NumDir) AS dirBallTimes FROM Uplans WHERE Semestr='1' GROUP BY NumDir
+	) AS P1,
+	(
+		SELECT Balls.NumSt, Students.NumGr, firstSem.NumDir, COUNT(Balls.NumSt) AS stuBallTimes
+			FROM Balls,
+				Students,
+				(SELECT * FROM Uplans WHERE Semestr='1') AS firstSem
+			WHERE firstSem.IdDisc=Balls.IdDisc AND Balls.NumSt=Students.NumSt
+			GROUP BY Balls.NumSt,Students.NumGr ,firstSem.NumDir
+	) AS P2
+	WHERE P1.dirBallTimes=P2.stuBallTimes AND P1.NumDir=P2.NumDir
+
+
+-- ★★★★ 统计每个班的人数
+SELECT NumGr, COUNT(NumGr) AS ballStuNumInEveryClass
+	FROM 
+	(
+		SELECT NumDir, COUNT(NumDir) AS dirBallTimes FROM Uplans WHERE Semestr='1' GROUP BY NumDir
+	) AS P1,
+	(
+		SELECT Balls.NumSt, Students.NumGr, firstSem.NumDir, COUNT(Balls.NumSt) AS stuBallTimes
+			FROM Balls,
+				Students,
+				(SELECT * FROM Uplans WHERE Semestr='1') AS firstSem
+			WHERE firstSem.IdDisc=Balls.IdDisc AND Balls.NumSt=Students.NumSt
+			GROUP BY Balls.NumSt,Students.NumGr ,firstSem.NumDir
+	) AS P2
+	WHERE P1.dirBallTimes=P2.stuBallTimes AND P1.NumDir=P2.NumDir
+	GROUP BY NumGr
+
+-- ★★★★★ 输出每个班级的人数
+SELECT Students.NumGr, COUNT(Students.NumGr) AS stuNumInEveryClass
+	FROM Students
+	GROUP BY Students.NumGr
+	
+
+-- 查找最终的班级
+SELECT DISTINCT P4.NumGr
+	FROM
+	(
+		SELECT NumGr, COUNT(NumGr) AS ballStuNumInEveryClass
+			FROM 
+			(
+				SELECT NumDir, COUNT(NumDir) AS dirBallTimes FROM Uplans WHERE Semestr='1' GROUP BY NumDir
+			) AS P1,
+			(
+				SELECT Balls.NumSt, Students.NumGr, firstSem.NumDir, COUNT(Balls.NumSt) AS stuBallTimes
+					FROM Balls,
+						Students,
+						(SELECT * FROM Uplans WHERE Semestr='1') AS firstSem
+					WHERE firstSem.IdDisc=Balls.IdDisc AND Balls.NumSt=Students.NumSt
+					GROUP BY Balls.NumSt,Students.NumGr ,firstSem.NumDir
+			) AS P2
+			WHERE P1.dirBallTimes=P2.stuBallTimes AND P1.NumDir=P2.NumDir
+			GROUP BY NumGr
+	) AS P4,
+	(
+		SELECT Students.NumGr, COUNT(Students.NumGr) AS stuNumInEveryClass
+			FROM Students
+			GROUP BY Students.NumGr
+	) AS P5
+	WHERE P4.ballStuNumInEveryClass=P5.stuNumInEveryClass
+
+
 --	4.9. Выбрать студентов, которые сдали все экзамены на хорошо и отлично.
 --	4.10. Выбрать студентов, которые сдали наибольшее количество экзаменов
